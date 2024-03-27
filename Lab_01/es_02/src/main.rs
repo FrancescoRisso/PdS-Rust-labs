2616e86b72edf2d3a67bde0091bcccba3ef43492
FrancescoRisso <francesco@frisso.net>
2024-03-27 10:37:07 +0100

    Added preparation step #3

--- Lab_01/es_02/src/main.rs
+++ Lab_01/es_02/src/main.rs
@@ -34,12 +34,47 @@ fn copy_repeat_file(file: &str) {
     }
 }
 
+struct Node {
+    name: String,
+    size: u32,
+    count: u32,
+}
+
+impl Node {
+    pub fn new(name: String) -> Self {
+        Node {
+            name,
+            size: 0,
+            count: 0,
+        }
+    }
+
+    pub fn size(mut self, size: u32) -> Self {
+        self.size = size;
+        self
+    }
+
+    pub fn count(mut self, count: u32) -> Self {
+        self.count = count;
+        self
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut tmp = self.name.clone();
+        tmp.push_str(format!(" size:{} count:{}", self.size, self.count).as_str());
+        tmp
+    }
+}
+
 fn main() {
-    use Error::*;
-    copy_repeat_file("../test.txt");
-    print_error(Simple(SystemTime::now()));
-    print_error(Complex(
-        SystemTime::now(),
-        "Boh I am testing stuff".to_string(),
-    ));
+    // use Error::*;
+    // copy_repeat_file("../test.txt");
+    // print_error(Simple(SystemTime::now()));
+    // print_error(Complex(
+    //     SystemTime::now(),
+    //     "Boh I am testing stuff".to_string(),
+    // ));
+
+    let node = Node::new("nodo".to_string()).size(10).count(5);
+    println!("{}", node.to_string())
 }
