96e0d2b6452785f3151f350401df1e4383c89c28
FrancescoRisso <francesco@frisso.net>
2024-03-27 10:47:25 +0100

    Commented all the preparation calls

--- Lab_01/es_02/src/main.rs
+++ Lab_01/es_02/src/main.rs
@@ -1,21 +1,23 @@
-use copy_repeat_file::copy_repeat_file;
-use node::Node;
-use print_error::{print_error, Error::*};
-use std::time::SystemTime;
+// use copy_repeat_file::copy_repeat_file;
+// use node::Node;
+// use print_error::{print_error, Error::*};
+// use std::time::SystemTime;
 
-mod copy_repeat_file;
-mod node;
-mod print_error;
+// mod copy_repeat_file;
+// mod node;
+// mod print_error;
 
-fn main() {
-    copy_repeat_file("../test.txt");
+// fn main() {
+//     copy_repeat_file("../test.txt");
 
-    print_error(Simple(SystemTime::now()));
-    print_error(Complex(
-        SystemTime::now(),
-        "Boh I am testing stuff".to_string(),
-    ));
+//     print_error(Simple(SystemTime::now()));
+//     print_error(Complex(
+//         SystemTime::now(),
+//         "Boh I am testing stuff".to_string(),
+//     ));
 
-    let node = Node::new("nodo".to_string()).size(10).count(5);
-    println!("{}", node.to_string())
-}
+//     let node = Node::new("nodo".to_string()).size(10).count(5);
+//     println!("{}", node.to_string())
+// }
+
+fn main() {}
