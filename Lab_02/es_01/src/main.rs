86da63bd9d74154f76aea6bdc3f0ac16df54ef9d
FrancescoRisso <francesco@frisso.net>
2024-04-12 11:25:20 +0200

    Added function to_slug

--- Lab_02/es_01/src/main.rs
+++ Lab_02/es_01/src/main.rs
@@ -43,7 +43,7 @@ fn main() {
     let s2 = "hello-slice";
     println!("{}", s1.is_slug()); // false
     println!("{}", s2.is_slug()); // true
-                                  // let s3: String = s1.to_slug();
-                                  // let s4: String = s2.to_slug();
-                                  // println!("s3:{} s4:{}", s3, s4); // stampa: s3:hello-string s4:hello-slice
+    let s3: String = s1.to_slug();
+    let s4: String = s2.to_slug();
+    println!("s3:{} s4:{}", s3, s4); // stampa: s3:hello-string s4:hello-slice
 }
--- Lab_02/es_01/src/my_slug.rs
+++ Lab_02/es_01/src/my_slug.rs
@@ -2,16 +2,25 @@ use crate::slugify;
 
 pub trait MySlug {
     fn is_slug(&self) -> bool;
+    fn to_slug(&self) -> String;
 }
 
 impl MySlug for String {
     fn is_slug(&self) -> bool {
         slugify(self) == *self
     }
+
+    fn to_slug(&self) -> String {
+        slugify(self)
+    }
 }
 
 impl MySlug for &str {
     fn is_slug(&self) -> bool {
         slugify(self) == *self
+    }
+
+    fn to_slug(&self) -> String {
+        slugify(self)
     }
 }
