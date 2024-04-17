77cf10d9c357201eb553f642c719523c735187a1
FrancescoRisso <francesco@frisso.net>
2024-04-17 10:52:31 +0200

    Create complex from (re, im), retreive real and imag part

--- Lab_02/es_02/src/lib.rs
+++ Lab_02/es_02/src/lib.rs
@@ -1,3 +1,20 @@
 pub mod solution {
-    pub struct ComplexNumber {}
+    pub struct ComplexNumber {
+        re: f32,
+        im: f32,
+    }
+
+    impl ComplexNumber {
+        pub fn new(real: f32, imag: f32) -> Self {
+            ComplexNumber { re: real, im: imag }
+        }
+
+        pub fn real(&self) -> f32 {
+            self.re
+        }
+
+        pub fn imag(&self) -> f32 {
+            self.im
+        }
+    }
 }
