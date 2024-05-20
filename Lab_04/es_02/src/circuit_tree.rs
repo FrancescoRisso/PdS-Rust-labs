eb98bfad93af8a178f56446efdc42222c727940b
FrancescoRisso <francesco@frisso.net>
2024-05-20 17:50:37 +0200

    Added tests

--- Lab_04/es_02/src/circuit_tree.rs
+++ Lab_04/es_02/src/circuit_tree.rs
@@ -1,14 +1,16 @@
 use crate::node::{Node, NodeLink};
-use std::marker::PhantomData;
+use std::collections::HashMap;
 
+#[derive(PartialEq, Debug)]
 pub struct CircuitTree {
     // choose the right type for root and names
-    root: PhantomData<Node>,
-    names: PhantomData<Node>,
+    root: NodeLink,
+    names: HashMap<String, NodeLink>,
 }
 
 impl From<&str> for CircuitTree {
     fn from(value: &str) -> Self {
+        _ = value;
         unimplemented!()
     }
 }
@@ -18,22 +20,37 @@ impl CircuitTree {
         unimplemented!()
     }
 
+    pub fn with_values(root: NodeLink, names: HashMap<String, NodeLink>) -> Self {
+        CircuitTree { root, names }
+    }
+
     // get a node by name
     pub fn get(&self, name: &str) -> NodeLink {
+        _ = self.root;
+        _ = self.names;
+        _ = name;
         unimplemented!()
     }
 
     // add a new node
     pub fn add(&mut self, parent_name: &str, node: Node) {
+        _ = parent_name;
+        _ = node;
         unimplemented!()
     }
 
     // is the light on? Error if it's not a light
-    pub fn light_status(&self, name: &str) -> Result<(), String> {
+    pub fn light_status(&self, name: &str) -> Result<bool, String> {
+        _ = name;
         unimplemented!();
     }
 
     pub fn turn_light_on(&self, name: &str) {
+        _ = name;
         unimplemented!();
+    }
+
+    pub fn get_node_names(&self) -> Vec<String> {
+        self.names.keys().cloned().collect()
     }
 }
--- Lab_04/es_02/src/node.rs
+++ Lab_04/es_02/src/node.rs
@@ -1,7 +1,9 @@
 use std::cell::RefCell;
-use std::marker::PhantomData;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
+use crate::circuit_tree::CircuitTree;
+
+#[derive(PartialEq, Debug)]
 pub enum NodeFunction {
     Generator(bool),
     Switch(bool),
@@ -9,26 +11,68 @@ pub enum NodeFunction {
 }
 
 pub type NodeLink = Option<Rc<RefCell<Node>>>;
+pub type NodeBackLink = Option<Weak<RefCell<Node>>>;
 
+#[derive(Debug)]
 pub struct Node {
     name: String,
     function: NodeFunction,
     // which type for parent?
     // PhantomData is just a placeholder to let it compile
-    parent: PhantomData<Node>,
+    parent: NodeBackLink,
     outs: [NodeLink; 2],
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name && self.function == other.function
+    }
 }
 
 impl From<&str> for Node {
     fn from(value: &str) -> Self {
+        _ = value;
         unimplemented!()
     }
 }
 
 impl Node {
+    pub fn new(
+        name: String,
+        function: NodeFunction,
+        parent: NodeBackLink,
+        outs: [NodeLink; 2],
+    ) -> Self {
+        Node {
+            name,
+            function,
+            parent,
+            outs,
+        }
+    }
+
+    pub fn without_links(name: String, function: NodeFunction) -> Self {
+        Node::new(name, function, None, [None, None])
+    }
+
     // turn on or off the switch or the generator, if it's a light return an error
     pub fn switch(&mut self) /*add return */
     {
         unimplemented!()
+    }
+
+    pub fn update_parent(&mut self, tree: CircuitTree) {
+        _ = tree;
+        unimplemented!()
+    }
+
+    pub fn get_status(&mut self) -> Option<bool> {
+        _ = self.parent;
+        _ = self.outs;
+        unimplemented!()
+    }
+
+    pub fn get_parent(&self) -> NodeLink {
+        self.parent.clone()?.upgrade()
     }
 }
