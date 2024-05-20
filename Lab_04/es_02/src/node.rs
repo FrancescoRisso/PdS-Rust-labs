1ad71e7fc4a0e4b84e77a055a632543a7116297c
FrancescoRisso <francesco@frisso.net>
2024-05-20 07:45:35 +0200

    Exercise preparation

---
+++ Lab_04/es_02/src/node.rs
@@ -0,0 +1,28 @@
+use std::cell::RefCell;
+use std::marker::PhantomData;
+use std::rc::Rc;
+
+pub enum NodeFunction {
+    Generator(bool),
+    Switch(bool),
+    Light,
+}
+
+pub type NodeLink = Option<Rc<RefCell<Node>>>;
+
+pub struct Node {
+    name: String,
+    function: NodeFunction,
+    // which type for parent?
+    // PhantomData is just a placeholder to let it compile
+    parent: PhantomData<Node>,
+    outs: [NodeLink; 2],
+}
+
+impl Node {
+    // turn on or off the switch or the generator, if it's a light return an error
+    pub fn switch(&mut self) /*add return */
+    {
+        unimplemented!()
+    }
+}
