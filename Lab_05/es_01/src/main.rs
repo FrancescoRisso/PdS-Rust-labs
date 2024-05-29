3358c769eaefc23312d2e469189671c5cdb51537
FrancescoRisso <francesco@frisso.net>
2024-05-29 13:21:23 +0200

    Input of the values

--- Lab_05/es_01/src/main.rs
+++ Lab_05/es_01/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// The array of numbers to be used
+    values: Vec<u8>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let nums = args.values;
+
+    println!("{:?}", nums);
 }
